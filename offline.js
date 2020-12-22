37fff743abff3630f59b5a43f8f8686030b4be47
Murasdil <45697682+murasdil@users.noreply.github.com>
2020-12-22 20:33:19 +0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1608647106,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/plosat.png",
+		"images/plaer-sheet0.png",
+		"images/pula-sheet0.png",
+		"images/myras-sheet0.png",
+		"images/healt.png",
+		"images/bom-sheet0.png",
+		"images/laser-sheet0.png",
+		"images/fair-sheet0.png",
+		"images/bombom-sheet0.png",
+		"images/old-sheet0.png",
+		"images/dont-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
